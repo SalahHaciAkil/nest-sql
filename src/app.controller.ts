4a029f17af49a0a55a4c612260906137b1ffac39
salah-mwizr <salah@mwizr.com>
2023-12-16 14:06:34 +0300

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get() decorator is a shorthand for @HttpCode(200) and @Header('Content-Type', 'text/html')
   @Get()
   getHello(): string {
     return this.appService.getHello();
